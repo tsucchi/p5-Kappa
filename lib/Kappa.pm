b1aa441c2fe955e0f0d810e0b46a83934478c7b6
Takuya Tsuchida <tsucchi@cpan.org>
2012-07-31 20:55:08 +0900

    version++

--- lib/Kappa.pm
+++ lib/Kappa.pm
@@ -2,7 +2,7 @@ package Kappa;
 use parent qw(SQL::Executor);
 use strict;
 use warnings;
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 use Class::Accessor::Lite (
     ro => ['dbh', 'row_namespace', 'table_namespace', 'options', 'table_name'],
     rw => ['id_generator'],
